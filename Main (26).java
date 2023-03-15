2eef8514c8e6cea5635d1b0b360d5af2c0fce365
Kanchan-Vaidya <127399929+Kanchan-Vaidya@users.noreply.github.com>
2023-03-15 13:45:06 +0530

    Add files via upload

---
+++ Main (26).java
@@ -0,0 +1,11 @@
+class pro
+{
+    public static void main (String args[])
+    {
+        int age=18;
+        if (age>=18)
+        System.out.println("You are eligible for vote");
+        else
+        System.out.println("You are not eligible for vote");
+    }
+}
